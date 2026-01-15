6f32222510ebfae0e103aee5c588523e21239401
bytepacket <153022036+bytepacket@users.noreply.github.com>
2026-01-15 15:20:51 -0500

    first commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const withNextra = require('nextra')({
+    experimental: {
+        turboMode: true,
+    },
+
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.tsx',
+    staticImage: false,
+});
+
+module.exports = withNextra({
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    trailingSlash: true,
+});
+
---
+++ theme.config.tsx
@@ -0,0 +1,11 @@
+import { DocsThemeConfig } from 'nextra-theme-docs';
+
+const config: DocsThemeConfig = {
+    sidebar: {
+        defaultMenuCollapseLevel: 1,
+        autoCollapse: true,
+    },
+    primaryHue: 252
+};
+
+export default config;
